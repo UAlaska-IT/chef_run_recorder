e111ff2ef5e0133dcb1516ff5577a1f2b936e0a1
Caleb Severn <calsev@gmail.com>
2018-04-11 17:06:06 -0800

    Added a handler for recording run stats

---
+++ files/default/last_run_recorder.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'chef/handler'
+
+module ChefRunRecorder
+  # It would be wonderful to use shared code, but these paths are redundant, so must be the same as in cookbook helpers
+  module Helper
+    def path_to_last_run_time
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_time.rb')
+    end
+
+    def path_to_last_run_node_name
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_node_name.rb')
+    end
+
+    def path_to_last_run_success_flag
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_success_flag.rb')
+    end
+
+    def write_run_time
+      File.open(path_to_last_run_time, 'w') do |file|
+        file.write Time.now.to_i
+      end
+    end
+
+    def write_node_name
+      File.open(path_to_last_run_node_name, 'w') do |file|
+        file.write node.name
+      end
+    end
+
+    def write_last_run_flag
+      File.open(path_to_last_run_node_name, 'w') do |file|
+        file.write
+        if run_status.success?
+          'true'
+        else
+          'false'
+        end
+      end
+    end
+  end
+
+  # A handler to cache run info for monitoring, MOTD, and ???
+  class LastRunRecorder < Chef::Handler
+    def report
+      write_run_time
+      write_node_name
+      write_last_run_flag
+    end
+  end
+end
