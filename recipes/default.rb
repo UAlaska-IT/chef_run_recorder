3ff88487bcb644ba6375a19e77d14061b93066c0
Caleb Severn <calsev@gmail.com>
2018-04-11 17:20:41 -0800

    Added a recipe for creating handler

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+cookbook_file ::File.join(node['chef_handler']['handler_path'], 'last_run_recorder.rb') do
+  source 'last_run_recorder.rb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+chef_handler 'ChefRunRecorder::LastRunRecorder' do
+  source ::File.join(node['chef_handler']['handler_path'], 'last_run_recorder.rb')
+  action :enable
+end
