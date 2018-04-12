c257bc5117feb8ef89d15ff7cc9fdeca0ada26ec
Caleb Severn <calsev@gmail.com>
2018-04-12 11:38:01 -0800

    No longer reporting in why-run mode

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Disable report handler in why-run mode
+if Chef::Config[:why_run]
+  Chef::Log.warn('skipping run recorder due to why-run mode')
+  return
+end
+
+# Write the handler code to the file system
 cookbook_file ::File.join(node['chef_handler']['handler_path'], 'last_run_recorder.rb') do
   source 'last_run_recorder.rb'
   owner 'root'
@@ -7,7 +14,9 @@ cookbook_file ::File.join(node['chef_handler']['handler_path'], 'last_run_record
   mode '0644'
 end
 
+# Enable report handler
 chef_handler 'ChefRunRecorder::LastRunRecorder' do
   source ::File.join(node['chef_handler']['handler_path'], 'last_run_recorder.rb')
+  supports report: true
   action :enable
 end
