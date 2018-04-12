d1cc562d516e2a186741561164cdd93304d8e50a
Caleb Severn <calsev@gmail.com>
2018-04-11 17:05:07 -0800

    Added helpers for record path

---
+++ libraries/chef_run_recorder.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module ChefRunRecorder
+  # This module implements shared utility code for consistency with dependent cookbooks
+  module Helper
+    def path_to_last_run_time
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_time.rb')
+    end
+
+    def path_to_last_run_node_name
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_node_name.rb')
+    end
+
+    def path_to_last_run_success_flag
+      return ::File.join(Chef::Config[:file_cache_path], 'last_chef_run_success_flag.rb')
+    end
+  end
+end
+
+Chef::Recipe.include(ChefRunRecorder::Helper)
+Chef::Resource.include(ChefRunRecorder::Helper)
