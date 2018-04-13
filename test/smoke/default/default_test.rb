3c4b36ef291015b31a5f21cff3d5fa6fd5c0e946
Caleb Severn <calsev@gmail.com>
2018-04-12 17:09:29 -0800

    Added tests for record files

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,1 +1,17 @@
 # frozen_string_literal: true
+
+describe file('/opt/chef/run_record/last_chef_run_time') do
+  it { should exist }
+end
+
+describe file('/opt/chef/run_record/last_chef_run_node_name') do
+  it { should exist }
+end
+
+describe file('/opt/chef/run_record/last_chef_run_success_flag') do
+  it { should exist }
+end
+
+describe file('/opt/chef/run_record/last_chef_run_exception') do
+  it { should exist }
+end
