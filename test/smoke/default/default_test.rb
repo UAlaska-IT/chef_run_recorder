96df302b881a0ef28f1e45359535b39760a14ac1
Caleb Severn <calsev@gmail.com>
2018-04-19 16:27:30 -0800

    Expanded file tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -2,16 +2,38 @@
 
 describe file('/opt/chef/run_record/last_chef_run_time') do
   it { should exist }
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its(:content) do
+    should match(/\d+/)
+  end
 end
 
 describe file('/opt/chef/run_record/last_chef_run_node_name') do
   it { should exist }
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
 
 describe file('/opt/chef/run_record/last_chef_run_success_flag') do
   it { should exist }
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its(:content) do
+    should match(/[true|false]/)
+  end
 end
 
 describe file('/opt/chef/run_record/last_chef_run_exception') do
   it { should exist }
+  it { should be_file }
+  it { should be_mode '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
