8276933bfe24f0e2905949ae3a6cd99354cc3d0e
Caleb Severn <calsev@gmail.com>
2018-09-09 09:00:39 -0800

    Updated Suse support in metadata

--- metadata.rb
+++ metadata.rb
@@ -18,7 +18,7 @@ supports 'centos', '>= 6.0'
 supports 'oracle', '>= 6.0'
 supports 'fedora'
 supports 'amazon'
-supports 'suse'
+# supports 'suse'
 # supports 'opensuse'
 supports 'freebsd', '>= 10.0'
 supports 'windows', '>= 6.1' # Windows 2008R2 or 7, see https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
