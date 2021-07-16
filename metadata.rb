8de2255221c4286106a4d081a36098e7c6cce0aa
Caleb Severn <calsev@gmail.com>
2021-07-15 17:45:42 -0700

    Fix cookstyle offenses

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,10 @@ maintainer 'OIT Systems Engineering'
 maintainer_email 'ua-oit-se@alaska.edu'
 license 'MIT'
 description 'Records the outcome of the last Chef run, for consumption by monitoring, MOTD, etc.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 git_url = 'https://github.com/ualaska-it/chef_run_recorder'
-source_url git_url if respond_to?(:source_url)
-issues_url "#{git_url}/issues" if respond_to?(:issues_url)
+source_url git_url
+issues_url "#{git_url}/issues"
 
 version '1.0.2'
 
@@ -22,6 +21,6 @@ supports 'fedora'
 supports 'amazon'
 supports 'suse'
 # supports 'opensuse'
-supports 'windows', '>= 6.1' # Windows 2008R2 or 7, see https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
+supports 'windows', '>= 6.3' # Windows 2012R2, see https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
 
-chef_version '>= 14.0' if respond_to?(:chef_version)
+chef_version '>= 14.0'
