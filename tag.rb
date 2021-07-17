38ea61d4cf75fb65c966068701ab1418c17d49b3
Caleb Severn <calsev@gmail.com>
2021-07-16 21:04:21 -0700

    Fix rubocop offenses

--- tag.rb
+++ tag.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 
 require 'open3'
 
 def tag
-  data = File.read('CHANGELOG.md').split('##')[1].split("\n").collect(&:strip).select {|line| line != ''}
+  data = File.read('CHANGELOG.md').split('##')[1].split("\n").collect(&:strip).reject { |line| line == '' }
   cmd = "git tag -a v#{data[0]} -m '#{data[1..].join('\n')}'"
-  stdout, stderr, _ = Open3.capture3(cmd)
+  stdout, stderr, = Open3.capture3(cmd)
   puts("#{stdout}#{stderr}")
 end
